72cd3e77ec49fed6d85a1d1137ae291688b1789d
zeallous <anson.zeall@gmail.com>
2015-08-19 14:39:38 +0700

    coderin90 first ex1
    First means exercise

---
+++ jsex1.js
@@ -0,0 +1,16 @@
+/*
+What will the code below output to the console and why?
+*/
+var Human = {
+    name: "Jerry",
+    printName: function() {
+        var self = this;
+        console.log("outer func:  this.name = " + this.name);
+        console.log("outer func:  self.name = " + self.name);
+        (function() {
+            console.log("inner func:  this.name = " + this.name);
+            console.log("inner func:  self.name = " + self.name);
+        }());
+    }
+};
+Human.printName();
